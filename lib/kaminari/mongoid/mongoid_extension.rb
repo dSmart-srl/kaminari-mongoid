5a63c9e8578082bed95114754845736b656c0f57
Sante Rotondi <saten.r@gmail.com>
2018-10-18 15:46:58 +0200

    tempfix

--- lib/kaminari/mongoid/mongoid_extension.rb
+++ lib/kaminari/mongoid/mongoid_extension.rb
@@ -8,14 +8,18 @@ module Kaminari
         extend ActiveSupport::Concern
         include Kaminari::ConfigurationMethods
 
+
         included do
-          scope Kaminari.config.page_method_name, Proc.new {|num|
-            limit(default_per_page).offset(default_per_page * ((num = num.to_i - 1) < 0 ? 0 : num))
-          } do
-            include Kaminari::Mongoid::MongoidCriteriaMethods
-            include Kaminari::PageScopeMethods
+          begin
+            scope Kaminari.config.page_method_name, Proc.new {|num|
+              limit(default_per_page).offset(default_per_page * ((num = num.to_i - 1) < 0 ? 0 : num))
+            } do
+              include Kaminari::Mongoid::MongoidCriteriaMethods
+              include Kaminari::PageScopeMethods
+            end
+          rescue 
           end
-        end
+        end 
 
         module ClassMethods
           def inherited(kls)
